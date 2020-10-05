61f981f5bf4ac6447ada958569425409a3922ce5
nikeo <nikeorever@gmail.com>
2020-10-05 17:20:25 +0800

    remove gradle portal publication

--- bytecode-writer/build.gradle.kts
+++ bytecode-writer/build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
     id("org.jetbrains.dokka")
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(Dependencies.asm)
 }
--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,45 +1,17 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka")
-
-    `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "0.12.0"
 }
 
 dependencies {
     implementation(project(":bytecode-writer"))
     implementation(Dependencies.jarTransformer)
     compileOnly(Dependencies.Android.gradlePlugin)
+    compileOnly(gradleApi())
 }
 
 repositories {
     google()
-    gradlePluginPortal()
-}
-
-// Use java-gradle-plugin to generate plugin descriptors and specify plugin ids
-gradlePlugin {
-    plugins {
-        create("reparaturPlugin") {
-            id = "cn.nikeo.reparatur"
-            implementationClass = "cn.nikeo.reparatur.gradle.ReparaturPlugin"
-        }
-    }
-}
-
-pluginBundle {
-    (plugins) {
-        "reparaturPlugin" {
-            // id is captured from java-gradle-plugin configuration
-            displayName = project.property("POM_NAME").toString()
-
-            website = project.property("POM_URL").toString()
-            vcsUrl = project.property("POM_SCM_URL").toString()
-
-            tags = listOf("Android", "Lollipop", "API 22", "5.1.1", "WebView", "Crash")
-            description = project.property("POM_DESCRIPTION").toString()
-        }
-    }
 }
 
 val dokkaDir = "${buildDir}/dokka"
